e74af4fd5cbc8bdd0b7e7915e022db2c9afdaf44
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-01 00:44:45 +0000

    Changed login fields

--- Assets/static/scripts/login.js
+++ Assets/static/scripts/login.js
@@ -2,8 +2,8 @@ function send_login_request(username, password_hashed) {
     let packet = {
         "type": 2, // TYPE_AUTHENTICATE
         "isFinal": true,
-        "Username": username,
-        "Password": password_hashed
+        "username": username,
+        "password": password_hashed
     };
 
     app.sock.onmessage = (e) => {
@@ -34,61 +34,7 @@ async function attempt_login() {
     send_login_request(username, await sha256(password));
 }
 
-async function create_new_user() {
-    let username = document.getElementById("create_user_username_entry").value;
-    let display_name = document.getElementById("create_user_displayname_entry").value;
-    let password = document.getElementById("create_user_password_entry").value;
-    let password2 = document.getElementById("create_user_password2_entry").value;
-
-    if (username == "" || display_name == "" || password == "" || password2 == "") {
-        document.getElementById("new-user-err-msg").innerHTML = "<div class=\"error-card\"><h2>Invalid entry</h2><p>Please ensure that non of the fields are empty</p></div>";
-        return;
-    }
-    else if (password != password2) {
-        document.getElementById("new-user-err-msg").innerHTML = "<div class=\"error-card\"><h2>Passwords do not match</h2><p>The passwords you have entered do not match, please try again</p></div>";
-        return;
-    }
-
-    app.sock.onmessage = (e) => {
-        let response = JSON.parse(e.data);
-        if (response.type == Application.PACKET_TYPE_ERROR) {
-            console.log(response);
-            document.getElementById("new-user-err-msg").innerHTML = "<div class=\"error-card\"><h2>" + response.name + "</h2><p>" + response.msg + "</p></div>";
-            return;
-        }
-        else {
-            send_login_request(response.Username, response.Password);
-        }
-    };
-
-    app.sock.send(JSON.stringify({
-        "type": Application.PACKET_TYPE_CREATE_NEW_USER,
-        "isFinal": true,
-        "Username": username,
-        "Password": await sha256(password),
-        "DisplayName": display_name
-    }));
-}
-
 function switch_to_new_user_page() {
     window.location = "https://data.nathcat.net/sso/?newUser";
     return;
-    // Creating new users is now delegated solely to AuthCat
-    app.page = new Page(Page.PAGE_CREATE_USER); app.load_page(() => {
-        document.getElementById('create_user_username_entry').onkeydown = (e) => {
-            if (e.key == 'Enter') create_new_user();
-        };
-        
-        document.getElementById('create_user_password_entry').onkeydown = (e) => {
-            if (e.key == "Enter") create_new_user();
-        };
-
-        document.getElementById('create_user_password2_entry').onkeydown = (e) => {
-            if (e.key == "Enter") create_new_user();
-        };
-
-        document.getElementById('create_user_displayname_entry').onkeydown = (e) => {
-            if (e.key == "Enter") create_new_user();
-        };
-    });
 }
