9d16204de16e56a8dbb2390b362a5005a4206b2e
Brooke <46959407+nimajnebec@users.noreply.github.com>
2024-11-05 11:15:56 +0000

    feat: begin work on homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import "greset";
+	import "$lib/style.css";
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container">
+	<div class="section">
+		<h1>Welcome to PeopleCat!</h1>
+		<p>This is a recreation of the PeopleCat webapp prototype made in Svelte!</p>
+	</div>
+	<div class="section login">
+		<input type="text" placeholder="Enter your Username" />
+		<input type="password" placeholder="Enter your Password" />
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 30px;
+		gap: 30px;
+	}
+
+	.login {
+		display: flex;
+		flex-direction: column;
+		gap: 5px;
+	}
+</style>
