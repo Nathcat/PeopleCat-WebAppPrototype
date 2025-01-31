cd4b867430770fc93c229635898f7de083cfd618
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-01-31 20:25:39 +0000

    feat: improve scroll pinning

--- src/routes/(navbar)/chat/[id]/+page.svelte
+++ src/routes/(navbar)/chat/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { application } from "$lib/application/application.svelte";
+	import { resize } from "svelte-resize-observer-action";
 	import { PacketType } from "$lib/application/packet";
 	import Message from "./Message.svelte";
 	import Input from "./Input.svelte";
@@ -8,49 +9,50 @@
 	let { data } = $props();
 
 	let messages = $derived(application.cache.messages[data.chat]);
-	let sticky = $state(true);
 	let margin = $state(0);
+	let scrollable = false;
 
 	onMount(() => {
-		const observer = new ResizeObserver(() => {
-			if (sticky) window.scrollTo(0, document.documentElement.scrollHeight);
-		});
-		observer.observe(document.documentElement);
-
-		const scroll = () => {
-			sticky = window.scrollY + window.innerHeight == document.documentElement.scrollHeight;
-		};
-		// todo: improve this as I still don't know exactly whats causing it (maybe message fetching)
-		setTimeout(() => window.addEventListener("scroll", scroll), 100);
-
 		// Request messages from the server if none
 		if (messages) return;
 		application.send({
 			type: PacketType.GET_MESSAGE_QUEUE,
 			payload: { chatId: data.chat },
 		});
-
-		return () => {
-			window.removeEventListener("scroll", scroll);
-			observer.disconnect();
-		};
 	});
+
+	function onResize() {
+		const nowScrollable = document.body.scrollHeight > document.body.clientHeight;
+		if (!CSS.supports("overflow-anchor", "none") || nowScrollable != scrollable)
+			window.scrollTo(0, document.documentElement.scrollHeight);
+		scrollable = nowScrollable;
+	}
 </script>
 
-<div class="message-container" style:margin-bottom="{margin}px">
+<div class="container" style:margin-bottom="{margin}px" use:resize={onResize}>
 	{#each messages as message}
 		<Message {message} />
 	{/each}
+	<div class="anchor"></div>
 </div>
 
 <Input chat={data.chat} bind:clientHeight={margin} />
 
 <style lang="scss">
-	.message-container {
+	.container {
 		flex-direction: column;
 		overflow-x: hidden;
 		display: flex;
 		padding: 10px;
 		gap: 10px;
+
+		:global(*) {
+			overflow-anchor: none;
+		}
+	}
+
+	.anchor {
+		overflow-anchor: auto;
+		height: 1px;
 	}
 </style>
